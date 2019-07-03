23862140ef5157613dfb6a56c698972de84ab9a3
eieikiki <51396982+eieikiki@users.noreply.github.com>
2019-07-03 13:54:46 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -44,7 +44,7 @@ def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
 #       TextSendMessage(text=event.message.text)
-        TextSendMessage(text="ควย")
+        TextSendMessage(text="สบายดีไหม")
     )
 
 
